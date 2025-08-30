960088e3cbc08eb25dc23d8611c71f8a56167b59
anonmaxyXMR <anonmaxy@proton.me>
2025-08-30 07:32:56 +0700

    first commit

---
+++ src/version.h
@@ -0,0 +1,72 @@
+#ifndef XMRIG_VERSION_H
+#define XMRIG_VERSION_H
+
+#define APP_ID        "core-gateway"
+#define APP_NAME      "core-gateway"
+#define APP_DESC      "Core Gateway Service"
+#define APP_VERSION   "1.0.0"
+#define APP_DOMAIN    "core-gateway.local"
+#define APP_SITE      "core-gateway.local"
+#define APP_COPYRIGHT "Copyright (C) 2016-2025 core-gateway project"
+#define APP_KIND      "proxy"
+
+#define APP_VER_MAJOR  1
+#define APP_VER_MINOR  0
+#define APP_VER_PATCH  0
+
+#ifdef _MSC_VER
+#   if (_MSC_VER >= 1930)
+#       define MSVC_VERSION 2022
+#   elif (_MSC_VER >= 1920 && _MSC_VER < 1930)
+#       define MSVC_VERSION 2019
+#   elif (_MSC_VER >= 1910 && _MSC_VER < 1920)
+#       define MSVC_VERSION 2017
+#   elif _MSC_VER == 1900
+#       define MSVC_VERSION 2015
+#   elif (_MSC_VER == 1800)
+#       define MSVC_VERSION 2013
+#   elif (_MSC_VER == 1700)
+#       define MSVC_VERSION 2012
+#   elif (_MSC_VER == 1600)
+#       define MSVC_VERSION 2010
+#   else
+#       define MSVC_VERSION 0
+#   endif
+#endif
+
+#ifdef XMRIG_OS_WIN
+#    define APP_OS "Windows"
+#elif defined XMRIG_OS_IOS
+#    define APP_OS "iOS"
+#elif defined XMRIG_OS_MACOS
+#    define APP_OS "macOS"
+#elif defined XMRIG_OS_ANDROID
+#    define APP_OS "Android"
+#elif defined XMRIG_OS_LINUX
+#    define APP_OS "Linux"
+#elif defined XMRIG_OS_FREEBSD
+#    define APP_OS "FreeBSD"
+#else
+#    define APP_OS "Unknown OS"
+#endif
+
+#define STR(X) #X
+#define STR2(X) STR(X)
+
+#ifdef XMRIG_ARM
+#   define APP_ARCH "ARMv" STR2(XMRIG_ARM)
+#else
+#   if defined(__x86_64__) || defined(__amd64__) || defined(_M_X64) || defined(_M_AMD64)
+#       define APP_ARCH "x86-64"
+#   else
+#       define APP_ARCH "x86"
+#   endif
+#endif
+
+#ifdef XMRIG_64_BIT
+#   define APP_BITS "64 bit"
+#else
+#   define APP_BITS "32 bit"
+#endif
+
+#endif // XMRIG_VERSION_H
